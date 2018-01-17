4829f1cf9371f93e03b9f2fae0ce142e2ba11263
Nourepide <nourepide@gmail.com>
2018-01-18 00:02:38 +0400

    Refactor getRoot

--- src/main/kotlin/net/ehvazend/graphics/getRoot.kt
+++ src/main/kotlin/net/ehvazend/graphics/getRoot.kt
@@ -2,7 +2,7 @@ package net.ehvazend.graphics
 
 import javafx.fxml.FXMLLoader
 
-fun <T> getRoot(address: String): T = FXMLLoader(Data::class.java.getResource(address)).let {
-    it.load<T>()
-    it.getRoot<T>()
+fun <T> getRoot(address: String): T = FXMLLoader(Data::class.java.getResource(address)).run {
+    load<T>()
+    getRoot<T>()
 }
