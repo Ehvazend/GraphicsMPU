9df027f4ae93d4c1f7a91e772bfc24ed9257d513
Nourepide <nourepide@gmail.com>
2018-01-18 00:21:04 +0400

    Fix back/next-ButtonEnable

--- src/main/kotlin/net/ehvazend/graphics/handlers/MoveBoxHandler.kt
+++ src/main/kotlin/net/ehvazend/graphics/handlers/MoveBoxHandler.kt
@@ -5,15 +5,15 @@ import net.ehvazend.graphics.handlers.AnimationHandler.Effect.toggleDisable
 import kotlin.properties.Delegates
 
 object MoveBoxHandler {
-    var backButtonEnable: Boolean by Delegates.observable(true) { _, oldValue, newValue ->
-        when (!oldValue) {
+    var backButtonEnable: Boolean by Delegates.observable(false) { _, oldValue, newValue ->
+        when (oldValue) {
             newValue -> return@observable
             else -> Data.backButton.toggleDisable()
         }
     }
 
-    var nextButtonEnable: Boolean by Delegates.observable(true) { _, oldValue, newValue ->
-        when (!oldValue) {
+    var nextButtonEnable: Boolean by Delegates.observable(false) { _, oldValue, newValue ->
+        when (oldValue) {
             newValue -> return@observable
             else -> Data.nextButton.toggleDisable()
         }
